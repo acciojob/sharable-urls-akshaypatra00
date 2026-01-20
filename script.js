1532c00c1596c43975150a65c9fb704309c2e2e3
Akshay patra <akshaykumarpatra00@gmail.com>
2026-01-20 20:59:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-// your code here
+ const form = document.getElementById("form");
+    const url = document.getElementById("url");
+
+    form.addEventListener("submit", function (e) {
+      e.preventDefault();
+
+      const name = document.getElementById("name").value;
+      const year = document.getElementById("year").value;
+
+      let baseUrl = "https://localhost:8080/";
+      let params = [];
+
+      if (name) params.push(`name=${name}`);
+      if (year) params.push(`year=${year}`);
+
+      if (params.length > 0) {
+        baseUrl += "?" + params.join("&");
+      }
+
+      url.textContent = baseUrl;
+    });
